a24212e6b96e0331ac6bfbc4cab8a9ca248321c4
xz2014 <zhangxinyu@pc-135-129.customer.hammersmith.generation.lan>
2017-02-22 12:12:00 +0000

    a

---
+++ draft.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import pandas as pd
+import math
+import random
+sys.path.append('/Users/zhangxinyu/Desktop/gibbs_sampler')
+import lda_sampler
+from lda_sampler import*
+
+
+num_topics = 3  # number of topics
+V = 10 # Vocabulary size V
+D = 5  # Document size D
+alpha = [0.001]*num_topics # Dirichlet prior for topic proportions
+xi = 30 # Poisson prior for 'number of words per document'
+eta = [0.01]*V # Dirichlet prior for topic-word distribution
+
+
+
+doc_term=G_P(num_topics,V,D,alpha,xi,eta)
+doc_term=doc_term.astype(np.int32)
